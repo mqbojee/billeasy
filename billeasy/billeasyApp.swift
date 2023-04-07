1d46a4dacfed2d7c5e71011ddc0403a2876562a8
yflliu <yflliu@tencent.com>
2023-04-07 14:42:21 +0800

    Initial Commit

---
+++ billeasy/billeasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  billeasyApp.swift
+//  billeasy
+//
+//  Created by YufengLiu on 2023/3/27.
+//
+
+import SwiftUI
+
+@main
+struct billeasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
